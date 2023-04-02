bc36cb6b00ea9fec69a527721117305d89deb483
Kene <kayyungx@gmail.com>
2023-04-02 20:44:10 +0100

    Add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Homepage from "./containers/Home";
 
 function App() {
-  return <div className="text-blue-400 main_font">Welcome</div>;
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
