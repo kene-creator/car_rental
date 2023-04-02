0a3a3c763ed891e5d82eaebe10dc0bf48cdb0787
Kene <kayyungx@gmail.com>
2023-04-02 20:23:00 +0100

    Add project main font

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="text-blue-400">Welcome</div>;
+  return <div className="text-blue-400 main_font">Welcome</div>;
 }
 
 export default App;
