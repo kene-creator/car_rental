3d6be0651ac3d7f2e27c766b81946848d603501d
Kene <kayyungx@gmail.com>
2023-04-02 20:44:31 +0100

    Add pages

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Navbar() {
+  return (
+    <div>
+      <div>
+        <div>
+          <a
+            href="/"
+            className="main_font font-bold text-[2rem] text-[#3563E9]"
+          >
+            MORENT
+          </a>
+        </div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
