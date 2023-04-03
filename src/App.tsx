f9eee515a7c08bb886e6642517bef70d62b34233
Kene <kayyungx@gmail.com>
2023-04-03 11:26:06 +0100

    update section wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import tw from "twin.macro";
 import "./App.css";
 import Homepage from "./containers/Home";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  ${tw`
+    w-full
+    h-full
+    flex
+    flex-col
+  `}
+`;
 
 function App() {
   return (
-    <div>
+    <AppContainer>
       <Homepage />
-    </div>
+    </AppContainer>
   );
 }
 
