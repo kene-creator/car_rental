8649bbf3b2495e51b19a4a3e3047774fe4645be2
Kene <kayyungx@gmail.com>
2023-04-04 22:27:56 +0100

    Add car interface

---
+++ src/components/car/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ICar } from "../../typings/car";
+
+interface ICarProps extends ICar {}
+
+export default function index() {
+  return <div>index</div>;
+}
---
+++ src/typings/car.ts
@@ -0,0 +1,11 @@
+export interface ICar {
+  name: string;
+  gearType: string;
+  gasTank: string;
+  passenger: string;
+  dailyPrice: string;
+  monthlyPrice: string;
+  discountPrice: string;
+  vehicle: string;
+  mileage: string;
+}
