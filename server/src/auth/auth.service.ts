4548aa01c0b1d0a8957c4630e61fcec284870aaa
Kene <kayyungx@gmail.com>
2023-04-10 14:31:29 +0100

    add signup function

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,14 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signin() {
-    return { msg: 'I have signed in' };
+
+  async signup(dto: AuthDto) {
+    //* generate the password hash
+    const hash = await argon2.hash(dto.password);
+
+    //* create the user in th db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+      select: {
+        id: true,
+        email: true,
+        createdAt: true,
+        updatedAt: true,
+        hash: false,
+      },
+    });
+
+    //* return the user
+    return user;
   }
 
-  signup() {
-    return { msg: 'I have signed up' };
+  signin() {
+    return { msg: 'I have signed in' };
   }
 }
