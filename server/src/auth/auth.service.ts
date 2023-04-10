5f346791adfec3c37f2eb57e3f25f5766e443d10
Kene <kayyungx@gmail.com>
2023-04-10 15:00:43 +0100

    handle signup error

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,33 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon2 from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signup(dto: AuthDto) {
-    //* generate the password hash
-    const hash = await argon2.hash(dto.password);
+    try {
+      //* generate the password hash
+      const hash = await argon2.hash(dto.password);
 
-    //* create the user in th db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-      select: {
-        id: true,
-        email: true,
-        createdAt: true,
-        updatedAt: true,
-        hash: false,
-      },
-    });
+      //* create the user in th db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+          updatedAt: true,
+          hash: false,
+        },
+      });
 
-    //* return the user
-    return user;
+      //* return the user
+      return user;
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          //* handle the error
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+      throw err;
+    }
   }
 
   signin() {
