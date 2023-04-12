a7184a01478aa6fa70610fb70e0a7f60db35f530
Kene <kayyungx@gmail.com>
2023-04-12 13:15:24 +0100

    Add car module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,6 +4,10 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { CarsModule } from './cars/cars.module';
 
 @Module({
   imports: [
@@ -13,7 +17,12 @@ import { PrismaModule } from './prisma/prisma.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
     PrismaModule,
+    CarsModule,
   ],
 })
 export class AppModule {}
