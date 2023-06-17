bbe3ba5db0114aaea2f59e9b03cd9976066063b0
Kene <kayyungx@gmail.com>
2023-06-17 18:31:09 +0100

    update car component

--- client/src/components/car/index.tsx
+++ client/src/components/car/index.tsx
@@ -4,7 +4,7 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import LocalGasStationIcon from "@mui/icons-material/LocalGasStation";
 import DirectionsCarFilledIcon from "@mui/icons-material/DirectionsCarFilled";
 import GroupIcon from "@mui/icons-material/Group";
-import Button from "../Button";
+import CarButton from "../CarButton";
 
 interface ICarProps extends ICar {}
 
@@ -22,47 +22,47 @@ export default function Car(props: ICarProps) {
     thumbnailSrc,
   } = props;
   return (
-    <div className="min-w-[17rem] min-h-[22rem] max-h-[22rem] flex flex-col items-center p-3 bg-white rounded-md main_font">
-      <div className="flex justify-between">
+    <div className="max-w-[17rem] min-h-[22rem] max-h-[22rem] flex flex-col items-center p-5 bg-white rounded-md main_font">
+      <div className="flex justify-between w-full">
         <div className="flex flex-col">
           <p className="text-[1rem] font-bold text-black">{name}</p>
-          <p className="text-[0.4rem] text-slate-400 font-normal">{vehicle}</p>
+          <p className="text-[0.65rem] text-slate-400 font-medium">{vehicle}</p>
         </div>
-        <FavoriteIcon sx={{ backgroundColor: "#ED3F3F" }} />
+        <FavoriteIcon sx={{ color: "#ED3F3F" }} />
       </div>
-      <div className="w-full h-auto">
+      <div className="w-full h-auto m-[2.5rem]">
         <img src={thumbnailSrc} alt="car" className="w-full h-full" />
       </div>
-      <div className="flex justify-between">
-        <div className="flex gap-1">
-          <LocalGasStationIcon sx={{ backgroundColor: "#90A3BF" }} />
-          <p className="text-[0.4rem] text-slate-400 font-normal">{gasTank}</p>
+      <div className="flex justify-between w-full">
+        <div className="flex gap-1 items-center">
+          <LocalGasStationIcon sx={{ color: "#90A3BF" }} />
+          <p className="text-[0.8rem] text-slate-400 font-normal">{gasTank}</p>
         </div>
-        <div className="flex gap-1">
-          <DirectionsCarFilledIcon sx={{ backgroundColor: "#90A3BF" }} />
-          <p className="text-[0.4rem] text-slate-400 font-normal">{gearType}</p>
+        <div className="flex gap-1 items-center">
+          <DirectionsCarFilledIcon sx={{ color: "#90A3BF" }} />
+          <p className="text-[0.8em] text-slate-400 font-normal">{gearType}</p>
         </div>
-        <div>
-          <GroupIcon sx={{ backgroundColor: "#90A3BF" }} />
-          <p className="text-[0.4rem] text-slate-400 font-normal">
+        <div className="flex gap-1 items-center">
+          <GroupIcon sx={{ color: "#90A3BF" }} />
+          <p className="text-[0.8rem] text-slate-400 font-normal">
             {passenger}
           </p>
         </div>
       </div>
-      <div>
+      <div className="flex items-center justify-between mt-4 w-full">
         <div className="flex flex-col">
           <p className="text-[1.6rem] text-black font-bold">
             {dailyPrice}/
-            <span className="text-[0.4rem] text-slate-400 font-normal">
+            <span className="text-[0.8rem] text-slate-400 font-normal">
               day
             </span>
           </p>
-          <p className="text-[0.6rem] text-slate-400 font-semibold line-through">
+          <p className="text-[1rem] text-slate-400 font-semibold line-through">
             {discountPrice}
           </p>
         </div>
         <div>
-          <Button>Rental Car</Button>
+          <CarButton>Rent Now</CarButton>
         </div>
       </div>
     </div>
