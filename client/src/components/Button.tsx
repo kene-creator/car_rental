2501681341bda7bc09c94b57cdc693687bb800bb
Kene <kayyungx@gmail.com>
2023-06-17 22:21:19 +0100

    update button components

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -2,13 +2,15 @@ import React from "react";
 
 type ButtonProps = {
   children: React.ReactNode;
+  onClick?: () => void;
 };
 
-const Button = ({ children }: ButtonProps) => {
+const Button = ({ children, onClick }: ButtonProps) => {
   return (
     <button
       type="button"
       className="bg-[#3563E9] py-2 px-1 text-white lg:w-[60%] font-semibold whitespace-nowrap"
+      onClick={onClick}
     >
       {children}
     </button>
