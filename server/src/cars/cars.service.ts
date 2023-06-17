54ee750aa3a10d4a182a1d875180d1221c666d9f
Kene <kayyungx@gmail.com>
2023-06-17 18:34:21 +0100

    update car service

--- server/src/cars/cars.service.ts
+++ server/src/cars/cars.service.ts
@@ -14,8 +14,23 @@ export class CarsService {
     });
   }
 
-  public async allCars(): Promise<Car[]> {
-    return this.prisma.car.findMany({});
+  // public async allCars(): Promise<Car[]> {
+  //   return this.prisma.car.findMany({});
+  // }
+
+  public async allCars(offset: number): Promise<Car[]> {
+    const totalCars = await this.prisma.car.count();
+    const remainingCars = totalCars - offset;
+    const take = remainingCars >= 8 ? 8 : remainingCars;
+
+    if (remainingCars < 8) {
+      return this.prisma.car.findMany({}); // Return all cars if remaining cars are less than 8
+    }
+
+    return this.prisma.car.findMany({
+      take, // Take the appropriate number of cars
+      skip: offset, // Skip the specified offset
+    });
   }
 
   async getPopularCars(): Promise<PopularCar[]> {
