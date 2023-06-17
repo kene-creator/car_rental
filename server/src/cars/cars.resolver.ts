a6c98b5901bd5a238b9640a6ae2fd62a84018220
Kene <kayyungx@gmail.com>
2023-06-17 18:33:51 +0100

    update car resolver

--- server/src/cars/cars.resolver.ts
+++ server/src/cars/cars.resolver.ts
@@ -1,15 +1,23 @@
 import { Resolver, Query, Args } from '@nestjs/graphql';
 import { Injectable } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { Car } from '@prisma/client';
 
 @Injectable()
 @Resolver()
 export class CarsResolver {
   constructor(private car: CarsService) {}
 
+  // @Query('cars')
+  // async getCars() {
+  //   return await this.car.allCars();
+  // }
+
   @Query('cars')
-  async getCars() {
-    return await this.car.allCars();
+  async getCars(
+    @Args('offset', { defaultValue: 0 }) offset: number,
+  ): Promise<Car[]> {
+    return await this.car.allCars(offset);
   }
 
   @Query('car')
