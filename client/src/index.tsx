89de2b9e550e5b3f0dd175b42db9f100ff64a935
Kene <kayyungx@gmail.com>
2023-06-18 23:47:35 +0100

    redux setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+// import { store } from "./app/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
+
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  PERSIST,
+  REHYDRATE,
+  PAUSE,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+import allCarsReducer from "./app/all_cars.state";
+import popularCarsReducer from "./app/popular_cars.state";
 
 import {
   createClient,
@@ -18,6 +34,23 @@ const client = createClient({
   exchanges: [cacheExchange, fetchExchange],
 });
 
+const rootReducer = combineReducers({
+  allCars: allCarsReducer,
+  popularCars: popularCarsReducer,
+});
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, PERSIST, REHYDRATE, PAUSE, PURGE, REGISTER],
+      },
+    }),
+});
+
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
@@ -25,13 +58,12 @@ root.render(
   <React.StrictMode>
     <UrqlProvider value={client}>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistStore(store)}>
+          <App />
+        </PersistGate>
       </Provider>
     </UrqlProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
