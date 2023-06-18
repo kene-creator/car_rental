8702bed97400d2afc7284bfd909a5ab8e69a0017
Kene <kayyungx@gmail.com>
2023-06-18 23:48:43 +0100

    setup store

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,7 +1,62 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  Reducer,
+  AnyAction,
+} from "@reduxjs/toolkit";
+
+interface Car {
+  id: string;
+  thumbnailSrc: string;
+  name: string;
+  gearType: string;
+  gasTank: string;
+  passenger: string;
+  dailyPrice: string;
+  monthlyPrice: string;
+  discountPrice: string;
+  vehicle: string;
+  mileage: string;
+}
+
+interface CarsState {
+  cars: Car[];
+}
+
+const initialState: CarsState = {
+  cars: [],
+};
+
+const carsReducer: Reducer<CarsState, AnyAction> = (
+  state = initialState,
+  action
+) => {
+  switch (action.type) {
+    case "ADD_CAR":
+      return {
+        ...state,
+        cars: [...state.cars, action.payload],
+      };
+    case "REMOVE_CAR":
+      return {
+        ...state,
+        cars: state.cars.filter((car) => car.id !== action.payload),
+      };
+    case "FETCH_ALL_CARS":
+      return {
+        ...state,
+        cars: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    cars: carsReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
