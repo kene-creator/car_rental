d3e05b412d85daff907a61459940f9ed83a67be2
Kene <kayyungx@gmail.com>
2023-06-21 17:54:14 +0100

    add method to verify paystack transaction

--- server/src/payment/payment.service.ts
+++ server/src/payment/payment.service.ts
@@ -45,10 +45,58 @@ export class PaymentService {
           reference: response.data.reference,
         },
       });
-      console.log(cuid());
       return response;
     } catch (error) {
       throw new Error('Failed to initialize Paystack transaction');
+    }
+  }
+
+  async verifyTransaction(reference: string): Promise<any> {
+    const secretKey = this.configService.get('PAYSTACK_SECRET_KEY');
+
+    const options = {
+      baseURL: 'https://api.paystack.co',
+      url: `/transaction/verify/${reference}`,
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${secretKey}`,
+      },
+    };
+
+    try {
+      const response = await firstValueFrom(
+        this.httpService.request(options).pipe(map((res) => res.data)),
+      );
+
+      await this.prisma.paystack_Transaction_Verification.create({
+        data: {
+          id: cuid(),
+          transactionId: response.data.id,
+          domain: response.data.domain,
+          status: response.data.status,
+          reference: response.data.reference,
+          amount: response.data.amount,
+          gateway_response: response.data.gateway_response,
+          paid_at: response.data.paid_at,
+          channel: response.data.channel,
+          currency: response.data.currency,
+          ip_address: response.data.ip_address,
+          attempts: response.data.log.attempts,
+          mobile: response.data.log.mobile,
+          success_status: response.data.log.success,
+          authorization_code: response.data.authorization.authorization_code,
+          card_type: response.data.authorization.card_type,
+          signature: response.data.authorization.signature,
+          first_name: response.data.customer.first_name,
+          last_name: response.data.customer.last_name,
+          email: response.data.customer.email,
+          customer_code: response.data.customer.customer_code,
+        },
+      });
+
+      return response;
+    } catch (error) {
+      throw new Error('Failed to verify Paystack transaction');
     }
   }
 }
