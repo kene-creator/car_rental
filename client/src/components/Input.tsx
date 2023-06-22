538b86c6531a7cbca86505e750452e09007e26cd
Kene <kayyungx@gmail.com>
2023-06-22 21:08:17 +0100

    update input component

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -4,6 +4,7 @@ interface InputProps {
   label: string;
   value: string;
   placeholder?: string;
+  type: string;
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
@@ -12,12 +13,13 @@ const InputComponent: React.FC<InputProps> = ({
   value,
   onChange,
   placeholder,
+  type,
 }) => (
   <div>
     <label className="font-bold">{label}</label>
     <input
       placeholder={placeholder}
-      type="text"
+      type={type}
       value={value}
       onChange={onChange}
       className="w-full py-3 pl-4 pr-12 text-gray-800 bg-[#e9eaea6b] rounded-md focus:outline-none"
