4d213e5c3cbfc1f6b635c95df07202c1bff2d0ad
Kene <kayyungx@gmail.com>
2023-06-22 21:11:33 +0100

    update style

--- client/src/containers/Payment/widgets/PaymentMethod.tsx
+++ client/src/containers/Payment/widgets/PaymentMethod.tsx
@@ -19,7 +19,7 @@ const PaymentMethod: React.FC<PaymentMethodProps> = ({
     <div className="bg-white rounded-lg p-5 main_font">
       <div className="pb-8">
         <h3 className="font-bold text-[1.3rem]">Payment Method</h3>
-        <div className="text-[#90A3BF] flex justify-between items-center">
+        <div className="text-[#90A3BF] flex justify-between items-center gap-4 text-[0.8rem] lg:text-[1rem]">
           <p>Please select your payment method</p>
           <p>Step 3 of 4</p>
         </div>
