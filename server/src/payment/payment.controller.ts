66e810e71b6150e04b1d7316e8b025a0dfa9b3c2
Kene <kayyungx@gmail.com>
2023-06-22 21:17:05 +0100

    Add payment verification endpoint

--- server/src/payment/payment.controller.ts
+++ server/src/payment/payment.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { PaymentService } from './payment.service';
 
 @Controller('payment')
@@ -16,5 +24,20 @@ export class PaymentController {
     );
 
     return transaction;
+  }
+
+  @Get('verify/:reference')
+  async verifyTransaction(@Param('reference') reference: string): Promise<any> {
+    try {
+      const transaction = await this.paystackService.verifyTransaction(
+        reference,
+      );
+      return transaction;
+    } catch (error) {
+      throw new HttpException(
+        'Failed to verify Paystack transaction',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
