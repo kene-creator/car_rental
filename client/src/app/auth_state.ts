b2e7fcfc49672c5b3451e9fec890d5a68fd6ef4c
Kene <kayyungx@gmail.com>
2023-06-26 23:33:54 +0100

    create auth state slice

---
+++ client/src/app/auth_state.ts
@@ -0,0 +1,43 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface AuthState {
+  accessToken: string | null;
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: AuthState = {
+  accessToken: null,
+  loading: false,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    loginStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    loginSuccess(state, action: PayloadAction<{ access_token: string }>) {
+      state.accessToken = action.payload.access_token;
+      state.loading = false;
+      state.error = null;
+    },
+    loginFailure(state, action: PayloadAction<string>) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    logout(state) {
+      state.accessToken = null;
+      state.loading = false;
+      state.error = null;
+    },
+  },
+});
+
+export const { loginStart, loginSuccess, loginFailure, logout } =
+  authSlice.actions;
+
+export default authSlice.reducer;
