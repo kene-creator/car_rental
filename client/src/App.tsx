a7d9aa03874bfc6c12a43f198e9a02c11d3258da
Kene <kayyungx@gmail.com>
2023-06-26 23:32:43 +0100

    update pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,8 @@ import Homepage from "./containers/Home";
 import styled from "styled-components";
 import PaymentPage from "./containers/Payment";
 import Dashboard from "./containers/Dashboard";
+import LoginPage from "./containers/Login_Registration/Login";
+import RegistrationPage from "./containers/Login_Registration/Registration";
 
 const AppContainer = styled.div`
   ${tw`
@@ -24,6 +26,8 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/payment/:carId" element={<PaymentPage />} />
           <Route path="/dashboard/:userId" element={<Dashboard />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/registration" element={<RegistrationPage />} />
         </Routes>
       </AppContainer>
     </BrowserRouter>
