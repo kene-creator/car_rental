106b51d2be2b1d6ce547c9849cac14f7486b9914
Kene <kayyungx@gmail.com>
2023-06-27 22:09:40 +0100

    update auth slice

--- client/src/app/auth_state.ts
+++ client/src/app/auth_state.ts
@@ -1,15 +1,33 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+export interface User {
+  id: string;
+  email: string;
+  emailvalid: boolean;
+  firstName: string;
+  lastName: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
 export interface AuthState {
   accessToken: string | null;
   loading: boolean;
   error: string | null;
+  registrationAccessToken: string | null;
+  isEmailVerified: boolean;
+  verification_token: string | null;
+  user: User | null;
 }
 
 const initialState: AuthState = {
   accessToken: null,
   loading: false,
   error: null,
+  registrationAccessToken: null,
+  isEmailVerified: false,
+  verification_token: null,
+  user: null,
 };
 
 const authSlice = createSlice({
@@ -20,10 +38,14 @@ const authSlice = createSlice({
       state.loading = true;
       state.error = null;
     },
-    loginSuccess(state, action: PayloadAction<{ access_token: string }>) {
+    loginSuccess(
+      state,
+      action: PayloadAction<{ access_token: string; user: User }>
+    ) {
       state.accessToken = action.payload.access_token;
       state.loading = false;
       state.error = null;
+      state.user = action.payload.user;
     },
     loginFailure(state, action: PayloadAction<string>) {
       state.loading = false;
@@ -33,11 +55,31 @@ const authSlice = createSlice({
       state.accessToken = null;
       state.loading = false;
       state.error = null;
+      state.user = null;
+    },
+    setRegistrationAccessToken(
+      state,
+      action: PayloadAction<{
+        access_token: string;
+        verification_token: string;
+      }>
+    ) {
+      state.registrationAccessToken = action.payload.access_token;
+      state.verification_token = action.payload.verification_token;
+    },
+    setEmailVerified(state, action: PayloadAction<boolean>) {
+      state.isEmailVerified = action.payload;
     },
   },
 });
 
-export const { loginStart, loginSuccess, loginFailure, logout } =
-  authSlice.actions;
+export const {
+  loginStart,
+  loginSuccess,
+  loginFailure,
+  logout,
+  setRegistrationAccessToken,
+  setEmailVerified,
+} = authSlice.actions;
 
 export default authSlice.reducer;
