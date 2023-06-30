e613303a3e6efebc5b6de5e1798aaa8f8320865f
Kene <kayyungx@gmail.com>
2023-06-30 23:17:47 +0100

    update initialize dto

--- server/src/payment/dto/index.ts
+++ server/src/payment/dto/index.ts
@@ -1,1 +1,2 @@
 export * from './initialize.dto';
+export * from './car.dto';
--- server/src/payment/dto/initialize.dto.ts
+++ server/src/payment/dto/initialize.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class InitializeDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   amount: string;
 }
