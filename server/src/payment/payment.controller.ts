d8532ab19241c429d522a382c1231867468f0201
Kene <kayyungx@gmail.com>
2023-07-01 23:03:26 +0100

    add payment webhook

--- server/src/payment/payment.controller.ts
+++ server/src/payment/payment.controller.ts
@@ -1,7 +1,7 @@
 import {
   Body,
   Controller,
-  Get,
+  HttpCode,
   HttpException,
   HttpStatus,
   Param,
@@ -21,33 +21,41 @@ export class PaymentController {
   async initializeTransaction(
     @Body() initializeDto: InitializeDto,
     @Param('userId') userId: string,
+    @Body() cars: CarDto[],
   ): Promise<any> {
     const transaction = await this.paystackService.initializeTransaction(
       initializeDto,
       userId,
+      cars,
     );
 
     return transaction;
   }
 
-  @Post('verify/:reference/:userId')
-  async verifyTransaction(
-    @Param('reference') reference: string,
-    @Param('userId') userId: string,
-    @Body() cars: CarDto[],
-  ): Promise<any> {
-    try {
-      const transaction = await this.paystackService.verifyTransaction(
-        reference,
-        userId,
-        cars,
-      );
-      return transaction;
-    } catch (error) {
-      throw new HttpException(
-        'Failed to verify Paystack transaction',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+  // @Post('/:reference/:userId')
+  // async verifyTransaction(
+  //   @Param('reference') reference: string,
+  //   @Param('userId') userId: string,
+  //   @Body() cars: CarDto[],
+  // ): Promise<any> {
+  //   try {
+  //     const transaction = await this.paystackService.verifyTransaction(
+  //       reference,
+  //       userId,
+  //       cars,
+  //     );
+  //     return transaction;
+  //   } catch (error) {
+  //     throw new HttpException(
+  //       'Failed to verify Paystack transaction',
+  //       HttpStatus.INTERNAL_SERVER_ERROR,
+  //     );
+  //   }
+  // }
+
+  @Post('verify')
+  @HttpCode(200)
+  async handleWebhook(@Body() payload: any): Promise<any> {
+    await this.paystackService.paystackWebhook(payload);
   }
 }
