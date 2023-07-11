46e64fb1713d8136addbd1a944652a4703b3b579
Kene <kayyungx@gmail.com>
2023-07-11 19:55:45 +0100

    update car module

--- server/src/cars/cars.module.ts
+++ server/src/cars/cars.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CarsResolver } from './cars.resolver';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { TimeoutInterceptor } from '@app/shared/interceptors/timeout.interceptors';
 
 @Module({
   imports: [],
-  providers: [CarsService, CarsResolver],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useFactory: () => {
+        const timeoutValue = 6000;
+        return new TimeoutInterceptor(timeoutValue);
+      },
+    },
+    CarsService,
+    CarsResolver,
+  ],
   exports: [CarsService],
 })
 export class CarsModule {}
