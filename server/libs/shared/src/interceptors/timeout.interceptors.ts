ff42069a268a83db253bd4c97ad040ca8397794e
Kene <kayyungx@gmail.com>
2023-07-23 22:46:28 +0100

    update timeout interceptor

--- server/libs/shared/src/interceptors/timeout.interceptors.ts
+++ server/libs/shared/src/interceptors/timeout.interceptors.ts
@@ -12,11 +12,13 @@ import { timeout, catchError } from 'rxjs/operators';
 export class TimeoutInterceptor implements NestInterceptor {
   constructor(private readonly timeoutMillis: number) {}
 
-  intercept(_: ExecutionContext, next: CallHandler): Observable<any> {
+  intercept(ctx: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       timeout(this.timeoutMillis),
       catchError((err) => {
         if (err instanceof TimeoutError) {
+          const request: Express.Request = ctx.switchToHttp().getRequest();
+          request['error'] = new RequestTimeoutException();
           return throwError(() => new RequestTimeoutException());
         }
         return throwError(() => err);
