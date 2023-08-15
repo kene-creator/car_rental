31abcdfbfd44bb04906855193a52e2c1e214253f
Kene <kayyungx@gmail.com>
2023-08-15 21:45:13 +0100

    update user controllers

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -3,8 +3,10 @@ import { GetUser } from '../auth/decorator';
 import { User } from '@prisma/client';
 import { AuthGuard } from '../auth/guard/auth.guard';
 import { UserInterceptor } from '../auth/interceptors/user.interceptor';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @UseGuards(AuthGuard)
+@ApiBearerAuth()
 @UseInterceptors(UserInterceptor)
 @Controller('users')
 export class UserController {
